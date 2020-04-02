e010a87fc4093e1c479b8ab1c76c69a26a6be5fb
SergiuChilat <chilatsergiu@gmail.com>
2020-04-02 15:28:34 +0300

    build

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@
         showForm: false,
         model: [],
         email: "",
-        formUrl: `http://area.info/api/get_form`,
+        //formUrl: `http://area.info/api/get_form`,
+        formUrl: `http://church-admin.loc/api/get_form`,
         orderCreateUrl: "http://church-admin.loc/api/payment/yandex/create",
         orderID: null
       };
